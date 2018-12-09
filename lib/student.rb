47e823c6e86aea2661e3d155739845249e9f31ce
Nicholas Rozmus <rozmusnicholas@gmail.com>
2018-12-09 02:21:50 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -1,3 +1,13 @@
 class Student 
-
+class Student < User
+ end 
+  def initialize
+    @knowledge=[]
+  end
+   def learn(string)
+    @knowledge<<string
+  end
+   def knowledge
+    @knowledge
+  end
 end
